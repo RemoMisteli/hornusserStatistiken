54a7e5bc268455a2b813e7e09fa1f6c7eb3fa93e
RemoMisteli <rmisteli@fluance.ch>
2022-03-28 21:37:59 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:RemoMisteli/hello-world.git testBranch:gh-pages
+git push -f git@github.com:RemoMisteli/hornusserStatistiken.git master:gh-pages
 cd 
